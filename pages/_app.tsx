2a8372895f350041b80665fe468d0f9c060ba5ad
Nathan Silva Santos <nathan@w16.com.br>
2022-02-18 21:53:13 -0300

    initial structure

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { theme, GlobalStyle } from "../style";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+export default MyApp;
