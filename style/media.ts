246b43d5505d8a14c4e22b166772627625d12b91
Nathan Silva Santos <nathan@w16.com.br>
2022-02-21 20:04:08 -0300

    update README.md

--- style/media.ts
+++ style/media.ts
@@ -16,11 +16,8 @@ const sizes = {
   bp400: 400,
 };
 
-// iterate through the sizes and create a media template
 export const media = Object.keys(sizes).reduce(
   (accumulator: Object, label: String): Sizes | {} => {
-    // use em in breakpoints to work properly cross-browser and support users
-    // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
     const emSize = sizes[label] / 16;
     accumulator[label] = (...args) => css`
       @media (min-width: ${emSize}em) {
