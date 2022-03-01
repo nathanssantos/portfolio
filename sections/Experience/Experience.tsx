35c29ba063fae4fce551a9bf45484e71fb712f00
Nathan Silva Santos <nathan@w16.com.br>
2022-03-01 05:07:27 -0300

    v1.0.0

--- sections/Experience/Experience.tsx
+++ sections/Experience/Experience.tsx
@@ -21,8 +21,8 @@ const Experience = () => {
                 image="/assets/images/rokkos.png"
                 url="https://web.rokkos.app"
                 description="Marketplace for photographers where they show, sell and deliver their photos during the event they're working. The platform also uses facial recognition to help customers find photos in which they are present."
-                work="I led the team that developed the application, built the entire front-end and helped the designers to create several interactions to improve usability and user's experience."
-                stack="React.js | Material UI"
+                work="I led the team that created Rokkos from scratch, built the entire front-end and helped the designers to create several features to improve usability and user's experience."
+                stack="React.js | MobX | Material UI | Sass"
                 width={110}
                 height={59}
               />
@@ -34,8 +34,8 @@ const Experience = () => {
                 image="/assets/images/tag.png"
                 url="https://ntag.com.br"
                 description="Transportadora Associada de Gás S.A. (TAG) operates in the natural gas transportation segment through gas pipelines. TAG owns and manages a significant portion of Brazil's natural gas transport assets, distributed among the North, Northeast and Southeast regions."
-                work="I developed the front-end of the institutional website taking into account the best SEO practices."
-                stack="WordPress"
+                work="I developed the front-end of TAG's institutional website taking into account the best SEO practices and following Engie's styleguide."
+                stack="WordPress | Sass"
                 width={110}
                 height={29}
               />
@@ -47,8 +47,8 @@ const Experience = () => {
                 image="/assets/images/lets-hike.webp"
                 url="https://play.google.com/store/apps/details?id=br.com.floripahike.app&hl=pt_BR&gl=US"
                 description="Mobile application that offers important information and helps the user to walk the trails of the city of Florianópolis and region safely."
-                work="I participated in the construction of the front-end and the design of several features of the application."
-                stack="React Native | Native Base"
+                work="I participated in the construction of Let's Hike and helped to improve several features of the application."
+                stack="React Native | Redux | Native Base"
                 width={110}
                 height={110}
               />
@@ -59,9 +59,9 @@ const Experience = () => {
                 name="Unne"
                 image="/assets/images/unne.png"
                 url="https://app.unnegestao.com.br"
-                description="Progressive web app used for analysis and management of growth strategies for small and medium businesses."
-                work="I developed the front-end of the application and helped the team to identify several improvements that should be applied throughout this process."
-                stack="Vue.js | Quasar Framework"
+                description="Platform used for analysis and management of growth strategies for small and medium businesses."
+                work="I developed the front-end of Unne's progressive web app and helped the team to identify several improvements that should be applied throughout this process."
+                stack="Vue.js | Quasar Framework | Sass"
                 width={110}
                 height={40}
               />
@@ -73,8 +73,8 @@ const Experience = () => {
                 image="/assets/images/turno-inverso.png"
                 url="https://play.google.com/store/apps/details?id=br.com.turnoinverso.aluno"
                 description="After-school activities platform for parents and guardians who want to explore their children's interests."
-                work="I led the team that built the application and also developed much of the front-end."
-                stack="React Native | Native Base"
+                work="I led the team that built Turno Inverso mobile app and also developed much of it's front-end."
+                stack="React Native | MobX | Native Base"
                 width={110}
                 height={110}
               />
@@ -86,8 +86,8 @@ const Experience = () => {
                 image="/assets/images/we-play.png"
                 url="https://play.google.com/store/apps/details?id=br.com.w16.letsplayapp"
                 description="Platform that allows teachers and arenas to manage classes and students in real time, making life easier for those who practice sports."
-                work="I was part of the team that created and developed the application."
-                stack="React Native | Native Base"
+                work="I was part of the team that created and developed We Play."
+                stack="React Native | MobX | Native Base"
                 width={110}
                 height={110}
               />
@@ -99,8 +99,8 @@ const Experience = () => {
                 image="/assets/images/plastubos.png"
                 url="http://plastubos.com.br"
                 description="Manufacturer of pipes and hydraulic connections present in the Brazilian market for over 20 years."
-                work="I was part of the team that built the institutional website developing much of the front-end."
-                stack="WordPress"
+                work="I was part of the team that built the company institutional website developing much of the front-end."
+                stack="WordPress | Bootstrap | Sass"
                 width={110}
                 height={110}
               />
